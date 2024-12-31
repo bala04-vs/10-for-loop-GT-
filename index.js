3eb3d3f59afbad82948b4be469e9dd57c9f47ef5
venkat posina <venkatposina29@gmail.com>
2024-12-31 10:47:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+
+
+
+
+// let str = "amma";
+// let y = "";  
+
+
+
+// for (let i = str.length - 1; i >= 0; i--) {
+//    y += str[i];
+// }
+
+// (str === y) ? console.log("pal") : console.log("not");
+
+
+// let x=["one","two","three","one"]
+
+// for(let i=0;i<x.length;i++){
+//     console.log(x[i][1])
+// }
+
+
+
+
+let x=["one","two","three","four"]
+let y="  "
+
+for(let i=x[2].length-1;i>=0;i--){
+  
+
+    
+
+    y+=x[i];
+
+}
+
+console.log(y)
+
+
+
+
+
